5e3e8474db88819b5e4756fdc0aa3dc9bdb3a3a3
JIEuNO <wisdom990629@naver.com>
2019-04-01 01:30:37 +0900

    my assignment

--- array_test.sh
+++ array_test.sh
@@ -1,1 +1,16 @@
-abc
+#!/bin/bash
+
+declare -a a
+i=0
+
+while true
+do
+	read -p "Enter a new word: " reply
+	case $reply in
+		list) echo ${a[*]};;
+		quit) exit 1;;
+		*)a[i]=$reply;
+		  let i++;
+		  echo "Length: $i";;
+	esac
+done
