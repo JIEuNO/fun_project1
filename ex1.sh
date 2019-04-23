d65ed74939a25610e8b9ac50c81064bda0bbb379
JIEuNO <wisdom990629@naver.com>
2019-04-23 23:27:19 +0900

    new upload

--- ex1.sh
+++ ex1.sh
@@ -1,1 +1,16 @@
-ABC
+#!/bin/bash
+
+declare -a newarray
+
+while [ TRUE ]
+do
+	read -p "Type the new word: " n
+	if [ "$n" == "quit" ]; then
+		exit 1
+	elif [ "$n" == "list" ]; then
+		echo ${newarray[*]}
+	else
+		newarray+=($n)
+		echo "Length:  ${#newarray[*]}"
+	fi
+done
